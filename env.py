28ab67c3ffcde486333e33510bf5bf1dbe9f238e
Tamoghno Kandar <55907205+tamoghnokandar@users.noreply.github.com>
2024-12-25 00:16:23 +0530

    Implement Halt Function

--- env.py
+++ env.py
@@ -41,25 +41,38 @@ class GridEnv(gym.Env):
         # Implement Logic of Transition
         tau_d = 2 # Block section from t_end+1 to t_end+tau_d and t_start-tau_d to t_start-1
         tau_arr = 1 # Block all other tracks of the destination station from t_end-tau_arr to t_end+tau_arr
-        tau_pre = 2  # Block the destination track from t_end-tau_pre to t_end-1.
-        list_tracks_end = [3, 4, 6]
-        time_section = section_length/speed
-        t_end = t_start + time_section
-        if action == "Move":
+        tau_pre = 2  # Block the destination track from t_end-tau_pre to t_end-1.  
+        tau_min = 3 # Minimum Dwelling Time     
+        list_tracks_end = [3, 4, 6]                                                                                                                                                                                                                                                                                      
+        
+        if action == 'move':
+            time_section = section_length/speed
+            t_end = t_start + time_section
             self.grid[start, t_start] = train_id # Starting track
             self.grid[connecting_edge, t_start:t_end+1] = train_id # Populate section
-
+            
             self.grid[connecting_edge, t_start-tau_d:t_start] = -1 # Block section from t_start-tau_d to t_start-1
             self.grid[connecting_edge, t_end+1:t_end+tau_d+1] = -1 # Block section from t_end+1 to t_end+tau_d
-
+            
             for track in list_tracks_end:
                 self.grid[track, t_end-tau_arr:t_end+tau_arr+1] = -1 # Block all tracks of the destination station from t_end-tau_arr to t_end+tau_arr
 
-            self.grid[end, t_end-tau_pre:t_end] = -1 # Block the destination track from t_end-tau_pre to t_end-1.
+            self.grid[end, t_end-tau_pre:t_end] = -1 # Block the destination track from t_end-tau_pre to t_end-1.      
             self.grid[end, t_end] = train_id # Ending track
         else:
-            # "Write code for halt action"
-            pass
+             # First time dwelling
+            if self.grid[start,t_start-1] != train_id:
+                self.grid[start, t_start:t_start+tau_min+1] = train_id
+            # If next section is free and it is not the first time halting do one step
+            elif start+1 < 41 and t_start+1 < self.num_time_steps and self.grid[start+1,t_start+1] == 0:
+                self.grid[start,t_start:t_start+2] = train_id
+            # If next section is not free find the timestep corresponding to when next section is available.
+            else:
+                row = self.grid[start+1] # Have to handle the out of bound case for start+1
+                # Search for the first non-zero column index from the t_start index onwards
+                non_zero_indices = (row[t_start+1:] != 0).nonzero(as_tuple=True)[0]
+                self.grid[start, t_start:non_zero_indices+2] = train_id           
+
 
  
     def reset(self):
