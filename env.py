561dd4dda81e0592a2b9fe1f651b13a836055cfd
Tamoghno Kandar <55907205+tamoghnokandar@users.noreply.github.com>
2025-01-02 14:37:42 +0530

    Update env.py

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
+
 import gym
 import torch
 import numpy as np
@@ -14,11 +15,11 @@ class GridEnv(gym.Env):
         self.args = args
         self.grid = self.create_network_grid(args)
         self.populate_trains(args)
-        
-        
+       
+       
     def create_network_grid(self,args):
         self.num_time_steps = args["max_time"] // args["time_step"] + 1
-        self.num_nodes = len(self.create_station_section_array(args)) 
+        self.num_nodes = len(self.create_station_section_array(args))
         state_tensor = torch.zeros(self.num_nodes,self.num_time_steps)
         return state_tensor
 
@@ -31,9 +32,9 @@ class GridEnv(gym.Env):
 
         for nm, ss, st in train_init_states:
             row = self.find_indices(self.GRID_ROW_INFO,ss)[0]
-            col = st//args['time_step'] 
+            col = st//args['time_step']
             state_tensor[row,col] = 1
-        
+       
         self.grid = state_tensor
         return state_tensor
 
@@ -41,23 +42,29 @@ class GridEnv(gym.Env):
         # Implement Logic of Transition
         tau_d = 2 # Block section from t_end+1 to t_end+tau_d and t_start-tau_d to t_start-1
         tau_arr = 1 # Block all other tracks of the destination station from t_end-tau_arr to t_end+tau_arr
-        tau_pre = 2  # Block the destination track from t_end-tau_pre to t_end-1.  
-        tau_min = 3 # Minimum Dwelling Time     
+        tau_pre = 2 # Block the destination track from t_end-tau_pre to t_end-1.  
+        tau_min = 3 # Minimum Dwelling Time    
         list_tracks_end = [3, 4, 6]  
         section_length = 1000
         speed = 100                                                                                                                                                                                                                                                                                    
         action = 'move'
 
         if action == 'move':
-            time_section = section_length//speed
+            time_section = section_length/speed
             t_end = t_start + time_section
+           
+            if (self.grid[connecting_edge, t_start:t_end+1] != 0).any()== True:
+                reward = -1 # Give negative reward
+                return
+
+           
             self.grid[start, t_start] = 1 # Starting track
             self.grid[connecting_edge, t_start:t_end+1] = 1 # Populate section
-            
+           
             self.grid[connecting_edge, t_start-tau_d:t_start] = 255 # Block section from t_start-tau_d to t_start-1
             self.grid[connecting_edge, t_end+1:t_end+tau_d+1] = 255 # Block section from t_end+1 to t_end+tau_d
 
-            
+           
             for track in list_tracks_end:
                 self.grid[track, t_end-tau_arr:t_end+tau_arr+1] = 255 # Block all tracks of the destination station from t_end-tau_arr to t_end+tau_arr
 
@@ -65,23 +72,33 @@ class GridEnv(gym.Env):
             self.grid[end,t_end+1] = 255
             self.grid[end, t_end] = 1 # Ending track
         else:
+         
              # First time dwelling
             if self.grid[start,t_start-1] != 1: # If previous time_step is not occupied by train(that is 1), do this
+                if (self.grid[start, t_start:t_start+tau_min+1] != 0).any()== True:
+                    reward = -1 # Give negative reward
+                    return
                 self.grid[start, t_start:t_start+tau_min+1] = 1
             # If next section is free and it is not the first time halting do one step
             elif start+1 < self.num_nodes and t_start+1 < self.num_time_steps and self.grid[start+1,t_start+1] == 0:
+                if (self.grid[start,t_start:t_start+2] != 0).any()== 0:
+                    reward = -1 # Give negative reward
+                    return
                 self.grid[start,t_start:t_start+2] = 1
             # If next section is not free find the timestep corresponding to when next section is available.
             else:
+                if (self.grid[start, t_start:non_zero_indices+2] != 0).any()== True:
+                    reward = -1 # Give negative reward
+                    return
                 row = self.grid[start+1] # Have to handle the out of bound case for start+1
                 # Search for the first non-zero column index from the t_start index onwards
                 non_zero_indices = (row[t_start+1:] != 0).nonzero(as_tuple=True)[0]
-                self.grid[start, t_start:non_zero_indices+2] = 1           
+                self.grid[start, t_start:non_zero_indices+2] = 1          
 
 
  
     def reset(self):
-        
+       
         return self.grid
 
     def render(self,):
@@ -93,7 +110,7 @@ class GridEnv(gym.Env):
         sections = args.get('sections', [])
         result = []
         num_sections = len(sections)
-        
+       
         for i, station_dict in enumerate(stations):
             station_name = next(iter(station_dict))
             capacity = station_dict[station_name].get('capacity', 1)
@@ -101,10 +118,10 @@ class GridEnv(gym.Env):
             if i < num_sections:
                 section_name = next(iter(sections[i]))
                 result.append(section_name)
-        
-        self.GRID_ROW_INFO = result 
+       
+        self.GRID_ROW_INFO = result
         return result
-    
+   
     def create_string_to_indices_map(self,result_array):
    
         mapping = defaultdict(list)
@@ -126,11 +143,11 @@ class GridEnv(gym.Env):
         mapping = self.create_string_to_indices_map(result_array)
         counters = defaultdict(int)
         indices = []
-        
+       
         for ss in ss_queries:
             index = self.get_next_index(mapping, counters, ss)
             indices.append(index)
-        
+       
         return indices
 
     def where_is_my_train(self,row,col):
@@ -139,23 +156,23 @@ class GridEnv(gym.Env):
 def create_graph(args, flag):
     if flag not in [1, -1]:
         raise ValueError("Flag must be either 1 (left-to-right) or -1 (right-to-left).")
-    
+   
     G = nx.MultiDiGraph()
-    
+   
     # Add section edges
     for section in args['sections']:
         for section_name, details in section.items():
             start = details['start']
             end = details['end']
             length = details['length']
-            
+           
             if flag == 1:
                 source, target = start, end
             else:
                 source, target = end, start
-            
+           
             G.add_edge(source, target, name=section_name, length=length)
-    
+   
     # Add station capacity edges (self-loops)
     for station in args['stations']:
         for station_name, attrs in station.items():
@@ -163,7 +180,7 @@ def create_graph(args, flag):
             for i in range(1, capacity + 1):
                 edge_name = f"{station_name}_l{i}"
                 G.add_edge(station_name, station_name, name=edge_name)
-    
+   
     return G
 
 def visualize_graph(G):
@@ -175,7 +192,7 @@ def visualize_graph(G):
     nx.draw_networkx_labels(G, pos, font_size=12, font_weight='bold')
     edge_labels = nx.get_edge_attributes(G, 'length')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red', font_size=3, label_pos=0.5)
-    
+   
 
     plt.axis('off')
     plt.title("Graph Visualization", fontsize=16)
@@ -199,7 +216,7 @@ def get_next_edges(G, current_pos):
                 edge_found = True
                 target_node = v
                 break
-        
+       
         if edge_found:
             outgoing_edges = G.out_edges(target_node, keys=True, data=True)
 
@@ -211,7 +228,7 @@ def get_next_edges(G, current_pos):
 
 
 if __name__ == "__main__":
-  
+ 
    
     args = {
         "stations": [
